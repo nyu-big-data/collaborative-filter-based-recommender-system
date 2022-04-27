653400ddec470554214e56e1ead72a23dc96918c
Akash Mishra <am11533@hlog-2.hpc.nyu.edu>
2022-04-27 16:02:05 -0400

    feat: Init Setup

--- data_loader.py
+++ data_loader.py
@@ -13,7 +13,7 @@ def process_movie_data(spark):
     links = spark.read.csv(f'{data_path}links.csv',header=True,schema='movieId STRING, imdbId STRING, tmdbId STRING')
     movies = spark.read.csv(f'{data_path}movies.csv',header=True,schema='movieId STRING, title STRING, genres STRING')
     ratings = spark.read.csv(f'{data_path}ratings.csv',header=True,schema='userId STRING,movieId STRING,rating FLOAT, timestamp STRING')
-    tags = spark.read.csv(f'{data_path}tags.csv',header=True,schema='userId STRING,movieId STRING,tag STRING, timestamp STRING')
+    tags = spark.read.csv(f'{data_path}tags.csv',header=True,schema='userId STRING,movieId STRING,tag STRING')
 
 
 
@@ -33,13 +33,15 @@ def process_movie_data(spark):
     tags.createOrReplaceTempView('tags')
 
     df = spark.sql('SELECT rt.userId,rt.movieId,mov.title,rt.rating FROM ratings rt JOIN movies mov ON rt.movieId=mov.movieId')
-    (training, val, test) = df.randomSplit([0.6, 0.2, 0.2])
+    df.repartition(10,'userId')
+    # df.sort('userId')
+    # (training, test) = df.randomSplit([0.8, 0.2])
     # training.write.parquet('training.parquet')
     # val.write.parquet('val.parquet')
     # test.write.parquet('test.parquet')
-    training.write.csv('training.csv')
-    val.write.csv('val.csv')
-    test.write.csv('test.csv')
+    # training.write.csv('training.csv')
+    # test.write.csv('test.csv')
+    print(df.rdd.getNumPartitions())
 
  
 if __name__ == "__main__":
--- recommender-train.py
+++ recommender-train.py
@@ -1,28 +1,63 @@
 from pyspark.sql import SparkSession
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.recommendation import ALS
-from pyspark.sql import Row
+from pyspark.ml.tuning import ParamGridBuilder, CrossValidator
 
-def train_model(spark):
-    # $example on$
-    training = spark.read.csv("./training.csv").rdd
-    testing = spark.read.csv("./testing.csv").rdd
+
+# from pyspark.sql import Row
+
+def train_model(spark, latentRanks, regularizationParams):
+    # training = spark.read.csv("./training.csv").rdd
+    # testing = spark.read.csv("./testing.csv").rdd
     
-    ratingsTrain = spark.createDataFrame(training)
-    ratingsTest = spark.createDataFrame(testing)
+    # ratingsTrain = spark.createDataFrame(training)
+    # ratingsTest = spark.createDataFrame(testing)
+    data_path = '/scratch/work/courses/DSGA1004-2021/movielens/ml-latest-small/'
+    ratings = spark.read.csv(f'{data_path}ratings.csv',header=True,schema='userId INT,movieId INT,rating FLOAT, timestamp STRING')
+    ratings.drop('timestamp')
+    (ratingsTrain, ratingsTest) = ratings.randomSplit([0.8, 0.2])
 
     # Build the recommendation model using ALS on the training data
     # Note we set cold start strategy to 'drop' to ensure we don't get NaN evaluation metrics
-    als = ALS(maxIter=5, regParam=0.01, userCol="userId", itemCol="movieId", ratingCol="rating",
-              coldStartStrategy="drop")
-    model = als.fit(ratingsTrain)
+    als = ALS(userCol="userId", itemCol="movieId", ratingCol="rating", nonnegative = True, implicitPrefs = False, coldStartStrategy="drop")
+    # model = als.fit(ratingsTrain)
 
-    # Evaluate the model by computing the RMSE on the test data
-    predictions = model.transform(ratingsTest)
-    evaluator = RegressionEvaluator(metricName="rmse", labelCol="rating",
-                                    predictionCol="prediction")
-    rmse = evaluator.evaluate(predictions)
-    print("Root-mean-square error = " + str(rmse))
+    # Add hyperparameters and their respective values to param_grid
+    param_grid = ParamGridBuilder().addGrid(als.rank, latentRanks).addGrid(als.regParam, regularizationParams).build()
+
+    # Define evaluator as RMSE 
+    evaluator = RegressionEvaluator(metricName="rmse", labelCol="rating", predictionCol="prediction") 
+    
+    # Build cross validation using CrossValidator
+    cv = CrossValidator(estimator=als, estimatorParamMaps=param_grid, evaluator=evaluator, numFolds=5)
+
+    #Fit cross validator to the training dataset
+    model = cv.fit(ratingsTrain)
+
+    #fetch the best model
+    best_model = model.bestModel
+    # print("Best Model - Rank:",best_model.getRank(), " RegParam:",best_model.getRegParam())
+    return best_model
+    
+def evaluate_test_pred(model):
+    test_pred = model.transform(test)
+    rmse = evaluator.evaluate(test_pred)
+    print(rmse)
+    return rmse,test_pred
+
+
+
+
+if __name__ == "__main__":
+    spark = SparkSession.builder.appName("Recommender-Model-GRP33").getOrCreate()
+
+    regularizationParams = [.01, .05, .1, .15]
+    latentRanks = [10, 50, 100, 150]
+
+    model = train_model(spark, latentRanks, regularizationParams)
+
+    evaluate_test_pred(model)
+
 
     # Generate top 10 movie recommendations for each user
     userRecs = model.recommendForAllUsers(10)
@@ -42,8 +77,3 @@ def train_model(spark):
     movieSubSetRecs.show()
 
     spark.stop()
-
-
-
-if __name__ == "__main__":
-    spark = SparkSession.builder.appName("Recommender-Model-GRP33").getOrCreate()
