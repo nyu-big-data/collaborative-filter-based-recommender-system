33ed4b3e3b185dca4957fbd8d22532819248bc71
Akash Mishra <am11533@hlog-1.hpc.nyu.edu>
2022-04-29 16:12:52 -0400

    popularity model

--- baseline_popularity.py
+++ baseline_popularity.py
@@ -11,7 +11,17 @@ def baseline_popularity_model(spark,netID,size_type,damping_factor):
 
     ratingsTrain.groupby('movieId','title').agg((sum('rating')/(count('rating')+damping_factor)).alias('pRatings')).show()
 
+def baseline_popularity_model_v2(spark,netID,size_type,damping_factor_global,damping_factor_item):
+    # Implementing R = u + bi + bu (popularity model)
 
+    schema = 'userId INT, movieId INT, rating FLOAT , timestamp INT, title STRING'
+    ratingsTrain = spark.read.csv(f'hdfs:/user/{netID}/movielens/{size_type}/training.csv' ,header=True ,schema=schema)
+    ratingsTest = spark.read.csv(f'hdfs:/user/{netID}/movielens/{size_type}/training.csv' ,header=True)
+    
+    # global_ratings = ratingsTrain.agg((sum('rating')/(count('rating')+damping_factor_global)).alias('global_ratings')).first()['global_ratings']
+    # ratingsTrain.groupby('movieId').agg(((sum('rating')- global_ratings)/(count('rating')+damping_factor_item)).alias('movieRatings')).show()
+    # ratingsTrain.groupby('userId').agg()
+    
 if __name__ == "__main__":
     spark = SparkSession.builder.appName("Baseline-Popularityå-GRP33").getOrCreate()
 
